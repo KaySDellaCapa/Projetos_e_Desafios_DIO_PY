7ae8f1c5ef48f7e13eb62895047fc41a5012033a
Kayque Silva Della Capa <ksdellacapa@gmail.com>
2023-08-05 11:52:25 -0300

    Delete desafios.py/intermediario directory

--- desafios.py/intermediario/aproveite_a_oferta.py
+++
@@ -1,9 +0,0 @@
-T = int(input())
-while T > 0:
-  T -= 1
-  N, K = input().split()
-  N = int(N)
-  K = int(K)
-  total = int(int(N % K) + int(N / K))
-   
-  print(total)
--- desafios.py/intermediario/dragao.py
+++
@@ -1,8 +0,0 @@
-C = int(input()) 
-
-for i in range (C): 
-    N = int(input())
-    if N <= 8000:
-      print("Inseto!")
-    else:
-      print("Mais de 8000!")
